edd801a237fcc77c5c71823d38079c2240f10959
Mdanson27 <149954637+Mdanson27@users.noreply.github.com>
2026-09-03 14:48:09 +0300

    style: load bid layout guardrails

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./product-pages.css";
 import "./workspace-pages.css";
 import "./mascot.css";
 import "./ingestion-status.css";
+import "./bid-quality.css";
 import { AuthProvider } from "@/components/auth/AuthProvider";
 
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
