95a40b859dbe7c4d6716a37d00c43404f4544b6f
Mdanson27 <149954637+Mdanson27@users.noreply.github.com>
2026-09-03 10:28:57 +0300

    test: cover eGP quotation and micro-procurement table

--- collector/tests/test_egp_source.py
+++ collector/tests/test_egp_source.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from datetime import datetime
 import unittest
 from zoneinfo import ZoneInfo
 
@@ -32,14 +31,29 @@ HTML = """
     </tr>
   </tbody>
 </table>
+<table>
+  <thead><tr>
+    <th>Procurement &amp; Disposal Entity</th><th>Procurement Method</th><th>Bid Details</th><th>Deadline</th><th>Opening Date/Time</th><th>Actions</th>
+  </tr></thead>
+  <tbody>
+    <tr>
+      <td><a href="/index/micro_egp">MoFA/NCONS/2026-2027/00012 Ministry of Foreign Affairs</a></td>
+      <td>Micro Procurement</td>
+      <td><span title="PROCUREMENT OF MOTOR VEHICLE REPAIR SERVICES">PROCUREMENT OF MOTOR...</span></td>
+      <td>2026-09-05</td>
+      <td>2026-09-05</td>
+      <td>View Details</td>
+    </tr>
+  </tbody>
+</table>
 </body></html>
 """
 
 
 class EGPUgandaSourceTests(unittest.TestCase):
-    def test_parses_standard_bid_notice_table(self):
+    def test_parses_standard_and_quotation_tables(self):
         bids = list(EGPUgandaSource().parse(HTML))
-        self.assertEqual(len(bids), 2)
+        self.assertEqual(len(bids), 3)
         first = bids[0]
         self.assertEqual(first.reference_number, "POU/NCONS/2026-2027/00199")
         self.assertEqual(first.organization, "Parliament of Uganda")
@@ -51,6 +65,14 @@ class EGPUgandaSourceTests(unittest.TestCase):
         self.assertEqual(first.deadline_precision, "date")
         self.assertEqual(first.deadline_at.tzinfo, ZoneInfo("Africa/Kampala"))
 
+        quotation = bids[2]
+        self.assertEqual(quotation.reference_number, "MoFA/NCONS/2026-2027/00012")
+        self.assertEqual(quotation.organization, "Ministry of Foreign Affairs")
+        self.assertEqual(quotation.procurement_type, "Micro Procurement")
+        self.assertEqual(quotation.title, "PROCUREMENT OF MOTOR VEHICLE REPAIR SERVICES")
+        self.assertIsNone(quotation.published_at)
+        self.assertEqual(str(quotation.sources[0].url), "https://egpuganda.go.ug/index/micro_egp")
+
     def test_reference_drives_cross_source_canonical_key(self):
         bid = list(EGPUgandaSource().parse(HTML))[0]
         self.assertEqual(bid.canonical_key(), "ref:pou/ncons/2026-2027/00199")
@@ -60,6 +82,16 @@ class EGPUgandaSourceTests(unittest.TestCase):
         self.assertEqual(bid.deadline_at.hour, 23)
         self.assertEqual(bid.deadline_at.minute, 59)
         self.assertEqual(bid.deadline_at.second, 59)
+
+    def test_deduplicates_same_reference_before_database(self):
+        raw = HTML.replace("</body></html>", "") + """
+        <table><thead><tr><th>Procuring Entity</th><th>Type</th><th>Subject of procurement</th><th>published</th><th>Deadline</th></tr></thead>
+        <tbody><tr><td>POU/NCONS/2026-2027/00199 Parliament of Uganda</td><td>Non Consultancy Services</td><td>Updated title</td><td>2026-08-19</td><td>2026-09-17</td></tr></tbody></table>
+        </body></html>"""
+        bids = list(EGPUgandaSource().parse(raw))
+        matching = [bid for bid in bids if bid.reference_number == "POU/NCONS/2026-2027/00199"]
+        self.assertEqual(len(matching), 1)
+        self.assertEqual(matching[0].deadline_at.day, 17)
 
     def test_ignores_unrelated_tables(self):
         raw = "<table><tr><th>Name</th></tr><tr><td>Nothing</td></tr></table>"
